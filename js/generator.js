d1d6f45b50a3d6faf24454c69c819e377a970175
Darkor-LF <76215906+Darkor-LF@users.noreply.github.com>
2020-12-19 20:07:55 +0100

    Update generator.js

--- js/generator.js
+++ js/generator.js
@@ -11,7 +11,7 @@ let motyIni = ['Mejor compañía',
     'Mejor pozo de horas',
     'Mejor bug',
     'Peor bug',
-    'Mejor apologia por el comunismo',
+    'Mejor apología por el comunismo',
     'Mejor salto temporal',
     'BAE',
     'Mejor vehículo volador',
@@ -23,10 +23,23 @@ let motyIni = ['Mejor compañía',
     'Peor juego de la década',
     'Mejor canción',
     'Escena de pelea',
-    'Escena de llorar'
-
+    'Escena de llorar',
+    'Mejor distopía',
+    'Peor distopía',
+    'Mejor apología por el capitalismo',
+    'Juego cyberpunk',
+    'JRPG',
+    'Shooter',
+    'Plataformas',
+    'Juego de acción',
+    'Juego de puzzles',
+    'Tycoon',
+    'Metroidvania',
+    'Roguelike',
+    'Mejor juego indie',
+    'Peor juego indie',
 ]
-
+ 
 let motyFin = ['que explota a la gente',
     'que lucha por la preservación',
     'que lucha contra la preservación',
@@ -46,9 +59,25 @@ let motyFin = ['que explota a la gente',
     'para acabar en la Audiencia Nacional',
     'con sectas',
     'porque los noventa fueron una cosa',
-    'de principios de los 2000'
-
+    'de principios de los 2000',
+    'por culpa de las TERFs',
+    'que tiene gatos',
+    'que tiene perros',
+    'desarrollado por nazis',
+    'desarrollado por comunistas bolcheviques bolivarianos',
+    'con señoras que te pisan la cara',
+    'con señores que son escoria pero te gustan',
+    'desarrollado por Hatsune Miku',
+    'que solo jugarías en la Game Boy Advance Tribal',
+    'que te brickea la PS4'
+    'donde sale el Papa',
+    'donde sale Rajoy',
+    'donde sale Rosalía',
+    'donde sale Jesucristo',
+    'donde sale la Reina de Inglaterra',
+    'que programó Taylor Swift una tarde de 2020 que estaba aburrida',
 ]
+
 
 
 const generateMoty = () => {
