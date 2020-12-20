560ff9a582983bcc8237e3a50fa02e7d6b1e53b7
Darkor-LF <76215906+Darkor-LF@users.noreply.github.com>
2020-12-20 16:51:00 +0100

    Update generator.js

--- js/generator.js
+++ js/generator.js
@@ -84,8 +84,17 @@ let motyFin = ['que explota a la gente',
 
 
 const generateMoty = () => {
-    let motyIniRand = motyIni[Math.floor(Math.random() * motyIni.length)]
-    let motyFinRand = motyFin[Math.floor(Math.random() * motyFin.length)]
+    const shuffle = (a)  => {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
+//     let motyIniRand = motyIni[Math.floor(Math.random() * motyIni.length)]
+//     let motyFinRand = motyFin[Math.floor(Math.random() * motyFin.length)]
+        let motyIniRand = shuffle(motyIni)
+  let motyFinRand = shuffle(motyFin)
 
 console.log(motyIni.length, Math.floor(Math.random() * motyIni.length))
         document.querySelector('.premio').innerHTML = `<i>${motyIniRand} ${motyFinRand}</i>`
